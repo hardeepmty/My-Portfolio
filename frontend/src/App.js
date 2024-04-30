063c22265e566ac156cab571f5ab3be32a67b98f
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-04-30 19:46:08 +0530

    few updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
       <Navbar/>
-      {/* <HeroPage/> */}
+      <HeroPage/>
       <Body/>
     </div>
   )
--- frontend/src/pages/Body.jsx
+++ frontend/src/pages/Body.jsx
@@ -1,11 +1,10 @@
-import { Stack } from '@mui/material'
 import React from 'react'
 
 const Body = () => {
   return (
-    <Stack sx={{bgcolor:"#f2f2f2", textAlign:"center",height:"100vh"}}>
-      <h1 style={{fontFamily:"Montserrat",marginTop:"100px"}}>HARDEEP</h1>
-    </Stack>
+    <div>
+      
+    </div>
   )
 }
 
