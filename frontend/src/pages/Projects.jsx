77251ec853b8738982d40f681c549b2760406c89
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-05-02 23:46:17 +0530

    projects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Projects = () => {
   return (
-    <div>
-      <h1 style={{color:"green"}}>Projects</h1>
+    <div
+      style={{
+        maxWidth: '600px',  // Corrected the syntax here
+        margin: '30px 10px',
+        padding: '15px',
+        gap: '10px',
+        display: 'flex',
+        borderRadius: '20px',
+        backgroundColor: '#101010',
+      }}
+    >
+      <h1 style={{ color: 'green' }}>Projects</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
