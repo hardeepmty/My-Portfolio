09aaf2a4c6adeca60f84b78d8009f7be9d2cbabc
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-05-03 00:17:14 +0530

    project section

---
+++ frontend/src/lib/data/works.jsx
@@ -0,0 +1,7 @@
+// data.js
+export const imagesData = [
+  '/path/to/image1.jpg',
+  '/path/to/image2.jpg',
+  '/path/to/image3.jpg',
+  // Add more image URLs here...
+];
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,19 +1,32 @@
+import { Typography } from '@mui/material';
 import React from 'react';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import './Projects.css';
 
 const Projects = () => {
   return (
-    <div
+    <div>
+    <div className='projects'
       style={{
-        maxWidth: '600px',  // Corrected the syntax here
+        maxWidth: '600px', 
         margin: '30px 10px',
+        top:"20px",
         padding: '15px',
         gap: '10px',
         display: 'flex',
         borderRadius: '20px',
         backgroundColor: '#101010',
+        textAlign:"center",
+        justifyContent:"center"
       }}
     >
-      <h1 style={{ color: 'green' }}>Projects</h1>
+      <Typography variant='h5' sx={{color:"#404040",fontWeight:"500", fontFamily:"Montserrat"}}><AccountTreeIcon sx={{fontSize:"large", color:"#A291FD"}}></AccountTreeIcon> Projects</Typography>
+    </div>
+    <div className="scroll-container">
+      {imagesData.map((image, index) => (
+        <img key={index} src={image} alt={`Image ${index}`} />
+      ))}
+    </div>
     </div>
   );
 };
