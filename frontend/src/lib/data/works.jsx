8e224a019546643665526cb518da16993f80f38e
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-05-03 20:06:53 +0530

    marquee added

---
+++ frontend/src/lib/data/works.jsx
@@ -0,0 +1,7 @@
+export const imageUrls = [
+  'images/icecream.png',
+  'images/illumina.png',
+  'images/pokemon.JPG',
+  'images/tastify.png',
+  'images/portfolio.png',
+];
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,28 +1,41 @@
-import { Typography } from '@mui/material';
 import React from 'react';
+import { Typography } from '@mui/material';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import './Projects.css';
+import { imageUrls } from "../lib/data/works"
 
 const Projects = () => {
   return (
     <div>
-    <div className='projects'
-      style={{
-        maxWidth: '600px', 
-        margin: '30px 10px',
-        top:"20px",
-        padding: '15px',
-        gap: '10px',
-        display: 'flex',
-        borderRadius: '20px',
-        backgroundColor: '#101010',
-        textAlign:"center",
-        justifyContent:"center"
-      }}
-    >
-      <Typography variant='h5' sx={{color:"#404040",fontWeight:"500", fontFamily:"Montserrat"}}><AccountTreeIcon sx={{fontSize:"large", color:"#A291FD"}}></AccountTreeIcon> Projects</Typography>
-    </div>
- 
+      <div className='projects'
+        style={{
+          maxWidth: '600px',
+          margin: '30px 10px',
+          padding: '15px',
+          gap: '10px',
+          display: 'flex',
+          flexDirection: 'column', 
+          borderRadius: '20px',
+          backgroundColor: '#101010',
+          textAlign: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        
+        <Typography variant='h5' sx={{ color: '#404040', fontWeight: '500', fontFamily: 'Montserrat' }}>
+          <AccountTreeIcon sx={{ fontSize: 'large', color: '#A291FD' }} />
+          Projects
+        </Typography>
+
+        
+        <div className="marquee-container">
+          <div className="marquee">
+            {imageUrls.map((url, index) => (
+              <img key={index} src={url} alt={`Image ${index + 1}`} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
